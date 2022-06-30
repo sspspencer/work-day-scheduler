80883582a5cc38c046d54eba64af8c1523147223
Spencer Pichette <ssptuningperformance@gmail.com>
2022-06-30 18:47:01 -0400

    add timed coloring and style work day blocks

--- Develop/script.js
+++ Develop/script.js
@@ -4,25 +4,56 @@ const scheduleContainer = document.getElementById("schedule-container");
 currentDay.innerHTML = n;
 
 const loadTimeBlocks = function () {
-  for (let i = 0; i < 9; i++) {
+  let num = 13;
+  const timeLabels = [
+    "9 AM",
+    "10 AM",
+    "11 AM",
+    "12 PM",
+    "1 PM",
+    "2 PM",
+    "3 PM",
+    "4 PM",
+    "5 PM",
+  ];
+  let currentHour = moment().format("LT");
+  let hour = currentHour.split(":");
+
+  for (let i = 0; i < timeLabels.length; i++) {
     const timeBlock = document.createElement("div");
-    timeBlock.className = "row time-block";
+    timeBlock.className = "row time-block ";
     scheduleContainer.appendChild(timeBlock);
     const time = document.createElement("p");
-    time.textContent = i;
-    time.className = "hour";
+    time.textContent = timeLabels[i];
+    time.className = "hour ";
     timeBlock.appendChild(time);
     const inputField = document.createElement("textarea");
+    inputField.id = i;
     timeBlock.appendChild(inputField);
     const saveBtn = document.createElement("button");
     saveBtn.className = "saveBtn";
-    saveBtn.textContent = "save";
+    const saveIcon = document.createElement("span");
+    saveIcon.textContent = "save";
+    saveIcon.className = "material-symbols-outlined";
+    saveBtn.appendChild(saveIcon);
     timeBlock.appendChild(saveBtn);
+
+    let timeCheck = timeLabels[i].split(" ");
+
+    if (timeCheck[0] == hour[0]) {
+      inputField.className = "present";
+      num = i;
+    }
+  }
+
+  for (let i = 0; i < timeLabels.length; i++) {
+    const inputField = document.getElementById(i);
+    if (i < num) {
+      inputField.className = "past";
+    } else if (i > num) {
+      inputField.className = "future";
+    }
   }
 };
 
 loadTimeBlocks();
-let hour = moment().format("LT");
-console.log(hour);
-let newHours = hour.split(":");
-console.log(newHours[0]);
