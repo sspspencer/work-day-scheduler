e478d1c6faa3f0c7cb90f3acde60ee3bb92ea775
Spencer Pichette <ssptuningperformance@gmail.com>
2022-06-30 16:12:43 -0400

    add work day blocks

--- Develop/script.js
+++ Develop/script.js
@@ -1,3 +1,28 @@
-const n = new Date();
+const n = moment().format("dddd MMMM Do YYYY");
 const currentDay = document.getElementById("currentDay");
-currentDay.innerHTML = n.toDateString();
+const scheduleContainer = document.getElementById("schedule-container");
+currentDay.innerHTML = n;
+
+const loadTimeBlocks = function () {
+  for (let i = 0; i < 9; i++) {
+    const timeBlock = document.createElement("div");
+    timeBlock.className = "row time-block";
+    scheduleContainer.appendChild(timeBlock);
+    const time = document.createElement("p");
+    time.textContent = i;
+    time.className = "hour";
+    timeBlock.appendChild(time);
+    const inputField = document.createElement("textarea");
+    timeBlock.appendChild(inputField);
+    const saveBtn = document.createElement("button");
+    saveBtn.className = "saveBtn";
+    saveBtn.textContent = "save";
+    timeBlock.appendChild(saveBtn);
+  }
+};
+
+loadTimeBlocks();
+let hour = moment().format("LT");
+console.log(hour);
+let newHours = hour.split(":");
+console.log(newHours[0]);
